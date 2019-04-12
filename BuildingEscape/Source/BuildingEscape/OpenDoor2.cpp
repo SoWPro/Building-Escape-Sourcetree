b333edeee9a4d77610368150d55170f3b781172f
SpawnofWar <jsotheractivity@yahoo.com>
2019-04-11 19:28:02 -0500

    So I got my door to open.  Primarily moveable mesh issue. Make sure moveable is sellected in unreal, when clicking on door.

--- BuildingEscape/Source/BuildingEscape/OpenDoor2.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor2.cpp
@@ -20,7 +20,7 @@ void UOpenDoor2::BeginPlay()
 	Super::BeginPlay();
 
 	AActor* Owner = GetOwner();
-	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+	FRotator NewRotation = FRotator(0.0f, -90.0f, 0.0f);
 	Owner->SetActorRotation(NewRotation);
 	// ...
 	
